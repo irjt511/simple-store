eeb42c4319967939630280747ab36fa8cafc90fa
محمة نشوان امين العواضي <mr.mohammed@MacBook-Air-mhmt.local>
2025-07-30 18:52:38 +0300

    تحديث الموقع

--- src/components/ProductsView.tsx
+++ src/components/ProductsView.tsx
@@ -20,8 +20,7 @@ export function ProductsView({ products, addToCart, viewProductDetails, category
           ← العودة للتصنيفات
         </button>
       </div>
-      
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8">
+      <div className="grid grid-cols-2 md:grid-cols-1 gap-8">
         {products.map((product: Product) => (
           <div key={product.id} className="product-card border border-blue-100 relative">
             {product.isSpecialOffer && (
